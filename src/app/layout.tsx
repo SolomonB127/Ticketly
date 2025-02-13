083c9d344d4755cb9885e879647874f18c332470
Akinsulire Olabode Solomon <solomonakinsulire@gmail.com>
2025-02-13 10:40:43 +0000

    fix: Removed unused Habibi declaration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Habibi } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { lazy, Suspense } from "react";
 const Navbar = lazy(() => import("./components/Navbar"));
